7fbe373f7fc5f2585bafed2907a7118867ebb18a
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-12 20:04:40 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - output lowercase and then uppercase alphabets
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
