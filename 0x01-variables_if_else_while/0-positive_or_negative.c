e088172a5495e9b8c9d28c11dcc94b1eb606aea3
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-12 18:56:31 -0800

    alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
  * main - assign a random number
  *
@@ -28,3 +28,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
+}
+
