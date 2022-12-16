2e6388908cb2da20678de94f3bed90c192fa0db6
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-16 15:45:46 -0800

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
  * print_numbers - print number from 0 - 9
  * Return: void
  */
-
 void print_numbers(void)
-
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
