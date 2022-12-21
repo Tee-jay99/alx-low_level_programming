3446637b0b1eed8a8f5ed8e93dd503678d84db59
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-20 16:10:12 -0800

    task 1-3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _puts - prints a string, followed by a new line
  * @str: pointer to print the string
@@ -8,9 +10,9 @@ void _puts(char *str)
 {
 	int insert;
 
-	for (insert = 0, str[insert] != '\0'; insert++)
+	for (insert = 0; str[insert] != '\0'; insert++)
 	{
-		_putchar(str[insert]);
+		_put(str[insert]);
 	}
 	_putchar('\n');
 }
