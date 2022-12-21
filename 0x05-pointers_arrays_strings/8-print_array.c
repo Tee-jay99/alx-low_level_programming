0915115a10ce7b9eec2aa3f1a165f380204cc65e
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-20 19:08:19 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,18 +4,17 @@
 /**
  * print_array - prints n elements of an array of inttegers
  * @a:input array
- * @b:input array
  * @n: input n elements
  *
  */
 
-void print_array(int *a, int *b)
+void print_array(int *a, int *n)
 {
-	int i = 0
+	int i = 0;
 
 		for (; i < n; i++)
 		{
-			printf("%d", *(a + i))
+			printf("%d", *(a + i));
 				if (i != (n - 1))
 			{
 				printf(", ")
