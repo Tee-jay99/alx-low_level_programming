2af62c705db91564e747a259f8af0e9e4cb895b6
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-23 17:26:12 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the contents of an array of  integers
+ * @a: the array of integers
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, insert;
+
+	for (insert = n - 1; insert >= n / 2; insert++)
+	{
+		tmp = a[n - 1 - insert];
+		a[n - 1 - insert] = a[insert];
+		a[insert] = tmp;
+	}
+}
