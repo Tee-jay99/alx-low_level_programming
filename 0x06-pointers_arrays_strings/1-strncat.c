0889d43dae6b20167707a1afa2b675e381ec9553
Tajudeen Muhammed <tajudeenmuhammed597@gmail.com>
2022-12-23 16:51:04 -0800

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,17 +4,18 @@
  * _strncat - concatenates two strings and use at most n bytes from @src
  *  @src: pointer string to be appended to dest
  *  @dest: pointer string that will be concatenated upon
+ *  @n: the number of bytes from src to be appended to dest
  *  Return: pointer to the resulting string dest
  */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int insert = 0, dest_len = 0;
 
 	while (dest[insert++])
 		dest_len++;
 
-	for (insert = 0; src[insert]; insert++)
+	for (insert = 0; src[insert] && insert < n; insert++)
 		dest[dest_len++] = src[insert];
 
 	return (dest);
